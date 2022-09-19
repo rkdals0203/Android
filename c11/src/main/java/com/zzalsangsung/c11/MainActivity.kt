11a4d9f44227efa0cb432aac90b197f4b891d2aa
rkdals0203 <rkdals0203@gmail.com>
2022-09-19 19:49:43 +0900

    Initial commit

---
+++ c11/src/main/java/com/zzalsangsung/c11/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.zzalsangsung.c11
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var visibleButton: Button
+    lateinit var invisibleButton: Button
+    lateinit var logoImageView: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        visibleButton = findViewById(R.id.visibleButton)
+        invisibleButton = findViewById(R.id.invisibleButton)
+        logoImageView = findViewById(R.id.logoImangeView)
+
+        visibleButton.setOnClickListener{
+            logoImageView.visibility = View.VISIBLE
+        }
+        invisibleButton.setOnClickListener{
+            logoImageView.visibility = View.INVISIBLE
+        }
+    }
+}
